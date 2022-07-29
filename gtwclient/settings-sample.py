f34bae3405e04407612da0179a0c285038adcdd3
Andrea Zucchelli <andrea.zucchelli@lepida.it>
2022-07-29 14:14:45 +0200

    settings con envirinemnt

--- gtwclient/settings-sample.py
+++ gtwclient/settings-sample.py
@@ -9,23 +9,29 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ
+import os
 from pathlib import Path
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True),
+    SECRET_KEY=(str,"django-insecure-%8hnm3b(k2!r$&p@2iuzte3xick)c!z9*l_di4m7&=_z4c&ae&")
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(BASE_DIR/'.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%8hnm3b(k2!r$&p@2iuzte3xick)c!z9*l_di4m7&=_z4c&ae&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
