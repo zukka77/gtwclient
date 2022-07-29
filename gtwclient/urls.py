bbeef6e15ffce7637bcc1cfc0cf7a2de48baae34
Andrea Zucchelli <andrea.zucchelli@lepida.it>
2022-07-29 15:36:40 +0200

    redirezione automatica a /client/validation

--- gtwclient/urls.py
+++ gtwclient/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.urls import re_path
+from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('client/',include('client.urls'))
+    path('client/',include('client.urls')),
+    re_path(r'^$',RedirectView.as_view(url='client/validation/',permanent=False))
 ]
