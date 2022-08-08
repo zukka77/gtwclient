786fbf1b3872b390a60877eecd7f1b78419a0aa9
Andrea Zucchelli <zukka77@gmail.com>
2022-08-08 15:46:12 +0200

    fix: create_pdf_with_attachment returns BytesIO

--- client/FSEgtwUtils/pdf.py
+++ client/FSEgtwUtils/pdf.py
@@ -10,7 +10,7 @@ def create_pdf(text="Test PDF"):
         pdf.cell(0,10,l,ln=1)
     return pdf.output(dest="S")
 
-def create_pdf_with_attachment(attachment:str=None,attachment_name="cda.xml",pdf_text:str=None):
+def create_pdf_with_attachment(attachment:str=None,attachment_name="cda.xml",pdf_text:str=None)->BytesIO:
     if pdf_text:
         pdf=create_pdf(pdf_text)
     else:
