c37f4bf4d683f16f9c92f6394c4febe16ddfd13f
Andrea Zucchelli <zukka77@gmail.com>
2023-02-15 12:20:21 +0100

    chore: update deps script

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 BASE_DIR=$(dirname $0)
 BOOTSTRAP_ITALIA_URL=https://github.com/italia/bootstrap-italia/releases/latest/download/bootstrap-italia.zip
 BOOTSTRAP_ITALIA_TMP=$(mktemp --suffix=bootstrapitalia.zip)
@@ -24,3 +24,8 @@ TMP_FILE=$(mktemp)
 curl -L https://unpkg.com/vue/dist/vue.global.prod.js > $TMP_FILE && mv $TMP_FILE $BASE_DIR/../client/static/client/vue/vue.js
 TMP_FILE=$(mktemp)
 curl -L https://unpkg.com/vue/dist/vue.global.js  > $TMP_FILE && mv $TMP_FILE $BASE_DIR/../client/static/client/vue/vue.devel.js
+
+TMP_DIR=$(mktemp -d)
+git clone --depth 1 https://github.com/ministero-salute/it-fse-support.git $TMP_DIR
+cp $TMP_DIR/doc/esempi/CDA/*.xml $BASE_DIR/../client/FSEgtwUtils/CDA/
+rm -rf $TMP_DIR
