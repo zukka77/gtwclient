da78e5914530b5eaa833d7166d8f4baedae2db77
Andrea Zucchelli <zukka77@gmail.com>
2023-06-08 22:34:15 +0200

    feat(utils): implement pdf pades signature

--- client/FSEgtwUtils/pdf.py
+++ client/FSEgtwUtils/pdf.py
@@ -1,6 +1,10 @@
 from fpdf import FPDF
 from pikepdf import Pdf, AttachedFileSpec
 from io import BytesIO
+from typing import BinaryIO
+from pyhanko.sign import signers
+from pyhanko.pdf_utils.incremental_writer import IncrementalPdfFileWriter
+from pyhanko import keys
 
 
 def create_pdf(text="Test PDF") -> str:
@@ -12,7 +16,7 @@ def create_pdf(text="Test PDF") -> str:
     return pdf.output(dest="S")
 
 
-def create_pdf_with_attachment(attachment: str = None, attachment_name="cda.xml", pdf_text: str = None) -> BytesIO:
+def create_pdf_with_attachment(attachment: str = None, attachment_name="cda.xml", pdf_text: str = None) -> BinaryIO:
     if pdf_text:
         pdf = create_pdf(pdf_text)
     else:
@@ -27,3 +31,17 @@ def create_pdf_with_attachment(attachment: str = None, attachment_name="cda.xml"
     pdfio.close()
     outpdf.seek(0)
     return outpdf
+
+
+def sign_pdf(cert: str, key: str, pdf: BinaryIO, key_pwd: bytes = None) -> BinaryIO:
+    cert = list(keys.load_certs_from_pemder_data(cert.encode("utf-8")))[0]
+    key = keys.load_private_key_from_pemder_data(key.encode("utf-8"), passphrase=key_pwd)
+    cms_signer = signers.SimpleSigner(signing_key=key, signing_cert=cert, cert_registry=None)
+    pdf.seek(0)
+    w = IncrementalPdfFileWriter(pdf)
+    out = signers.sign_pdf(
+        w,
+        signers.PdfSignatureMetadata(field_name="Signature1"),
+        signer=cms_signer,
+    )
+    return out
